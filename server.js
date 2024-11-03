b604c251cb7c8716753cc89686afaf2ae864c51c
colinguinane1 <colinguinane@gmail.com>
2024-11-03 14:54:49 -0700

    added vercel json

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
-app.listen(3000, () => {console.log('Server is running on port 3000')});
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get('/', (req, res) => {res.send('Hello World!')});
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
