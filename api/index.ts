c65f08a27894fe6f2cc515df047282a31a943aa4
colinguinane1 <colinguinane@gmail.com>
2024-11-03 20:10:44 -0700

    mongodb

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from "express";
-import { mongo } from "mongoose";
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config();
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected to MongoDB.")).catch((err: any) => console.log(err));
 
 app.get("/", (req: Request, res: Response) => res.send("Express on Vercel"));
 
