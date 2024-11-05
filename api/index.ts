45b0b3b28624ff9be5643c68c2364b6bbd1d0dab
colinguinane1 <colinguinane@gmail.com>
2024-11-05 16:48:01 -0700

    api work

--- api/index.ts
+++ api/index.ts
@@ -2,17 +2,24 @@ import { Request, Response } from "express";
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const numberRoutes = require("../routes/Numbers")
+const cors = require("cors");
+const numberRoutes = require("../routes/Numbers");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected to MongoDB.")).catch((err: any) => console.log(err));
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(
+    () => console.log("Connected to MongoDB."),
+    app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`))
+  )
+  .catch((err: any) => console.log(err));
 
 app.get("/", (req: Request, res: Response) => res.send("API is running. "));
 
+app.use("/api/numbers", numberRoutes);
 
-
-app.use("/api/numbers", numberRoutes)
-app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
-module.exports = app
+module.exports = app;
