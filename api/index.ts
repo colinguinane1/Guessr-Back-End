7b6786a933f9e80a0c956a3c0030b2b905331fae
colinguinane1 <colinguinane@gmail.com>
2024-11-21 16:58:55 -0700

    cors fix attempt 3

--- api/index.ts
+++ api/index.ts
@@ -30,5 +30,3 @@ app.get("/", (req: Request, res: Response) => res.send("API is running"));
 app.use("/api/numbers", numberRoutes);
 
 module.exports = app;
-
-// num-game-front-nrhsm1r2p-colinguinane1s-projects.vercel.app/:1 Access to fetch at 'https://num-game-back-end.vercel.app/api/numbers/current' from origin 'https://num-game-front-nrhsm1r2p-colinguinane1s-projects.vercel.app' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
