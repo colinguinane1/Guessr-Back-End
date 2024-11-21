5e0638abf4abc92e12ff0030d492ff63fac364a9
colinguinane1 <colinguinane@gmail.com>
2024-11-21 16:31:01 -0700

    fixed win detection

--- api/index.ts
+++ api/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({ origin: 'https://num-game-front-end.vercel.app' }));
+app.use(cors({ origin: ['https://num-game-front-end.vercel.app', 'http://localhost:3000'] }));
 
 mongoose
   .connect(process.env.MONGO_URI)
